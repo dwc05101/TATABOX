e2d44e9390efce936ae83fd26803eb1498cb8f4b
공광조 <admin@Ownerui-MacBook-Pro.local>
2019-04-29 01:23:47 +0900

    Updates

---
+++ precache-manifest.9d4eb54d55f0a32037eb5073167a2f71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117c69e55dad665781fb8d5b434f1dea",
+    "url": "/TATABOX/index.html"
+  },
+  {
+    "revision": "71c89f56981ade4c31ba",
+    "url": "/TATABOX/static/css/main.0703a124.chunk.css"
+  },
+  {
+    "revision": "2101777af894817710bf",
+    "url": "/TATABOX/static/js/2.23988cea.chunk.js"
+  },
+  {
+    "revision": "71c89f56981ade4c31ba",
+    "url": "/TATABOX/static/js/main.6b5b58ea.chunk.js"
+  },
+  {
+    "revision": "6665888ba45a7f3187e7",
+    "url": "/TATABOX/static/js/runtime~main.58dbc0cd.js"
+  },
+  {
+    "revision": "613e4c158450d8bbd517fe4f2c9f5167",
+    "url": "/TATABOX/static/media/bg_with_text.613e4c15.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TATABOX/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d264d5c8b138c773cdfa9edd337463b.js"
+  "/TATABOX/precache-manifest.9d4eb54d55f0a32037eb5073167a2f71.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TATABOX/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

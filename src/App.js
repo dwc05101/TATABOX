12ec98fe82470ca6c383a75b53d0cc53ee8ef21a
guseul <hbbr@kaist.ac.kr>
2019-05-23 23:59:53 +0900

    upload csv file to add students list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import Main from "./components/main_component";
 import MakeClass from "./components/make_class_component";
-import Outline from './components/OutLinedTextFields'
 import Management from './components/student_manage_component';
 import AttendanceCheck from './components/attendance_check';
 import GradeReport from "./components/grade_report_component";
@@ -11,6 +10,7 @@ import StudentSide from "./components/student_side";
 
 import students from "./data/student_pairs";
 import Firebase from './firebase';
+import UploadCsv from './components/uploadCsv.js';
 
 class App extends Component{
   constructor(props){
@@ -28,9 +28,6 @@ class App extends Component{
         <Route path="/class"
           render = {props => <MakeClass Firebase = {fb} />}
         ></Route>
-        <Route path="/test"
-          component={props => <Outline/>}
-        ></Route>
         <Route path="/management/:classname"
           component = {props=><Management Firebase={fb} students={students} {...props}/>}
         ></Route>
@@ -42,6 +39,9 @@ class App extends Component{
         ></Route>
         <Route path="/student/:classname/:date"
           component={props => <StudentSide Firebase={fb} {...props}/>}
+        ></Route>
+        <Route path="/test"
+          component={props => <UploadCsv Firebase={fb} {...props}/>}
         ></Route>
       </Router>
     );

33cc79f103d735f80750de8113ac56a08195627c
guseul <hbbr@kaist.ac.kr>
2019-05-25 16:49:35 +0900

    upload csv

--- src/components/uploadCsv.js
+++ src/components/uploadCsv.js
@@ -3,28 +3,17 @@ import Button from '@material-ui/core/Button';
 import XLSX from 'xlsx';
 import ReactFileReader from 'react-file-reader'
 import { arrayExpression } from '@babel/types';
+import Paper from '@material-ui/core/Paper';
+import './uploadCsv.css'
+import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
     margin: {
       margin: theme.spacing.unit,
     },
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    textField: {
-      marginLeft: theme.spacing.unit,
-      marginRight: theme.spacing.unit,
-    },
-    dense: {
-      marginTop: 16,
-    },
-    menu: {
-      width: 200,
-    },
   });
 
-export default class UploadCsv extends React.Component {
+class UploadCsv extends React.Component {
     constructor(props){
         super(props);
         this.handleFiles = this.handleFiles.bind(this);
@@ -33,25 +22,12 @@ export default class UploadCsv extends React.Component {
             filename: '',
             preview : [],
             imgfiles : [],
+            tests: [],
         }
     }
 
     handleImages = files => {
-      let that = this;
-      let reader = new FileReader();
       this.setState({imgfiles:files});
-      // let file = files[0];
-      // console.log(`files`,files);
-      // console.log(`files length`,files.length);
-      // if (file !== null) {
-      //     reader.onloadend = () => {
-      //         that.setState({
-      //             selectedFile: file,
-      //             imgPreviewUrl: reader.result
-      //         });
-      //     }
-      //     reader.readAsDataURL(file)
-      // }
     }
 
     handleFiles = files => {
@@ -74,12 +50,14 @@ export default class UploadCsv extends React.Component {
               jsonData['name']=row[1];
               jsonData['email']=row[2];
               jsonData['dept']=row[3];
-              let e = <tr>
-                        <th>{row[0]}</th>
-                        <th>{row[1]}</th>
-                        <th>{row[3]}</th>
-                        <th>{row[2]}</th>
-                      </tr>
+              let e =<Paper id = 'row_student'>
+                        <div class='wrap'>
+                            <div class = "cell_sid">{row[0]}</div>
+                            <div class = "cell_name">{row[1]}</div>
+                            <div class = "cell_dept">{row[3]}</div>
+                            <div class = "cell_email">{row[2]}</div>
+                        </div>
+                      </Paper>
               if(row[4].length>0) jsonData['imgpath']=row[0]+'.'+row[4];
               else jsonData['imgpath']='';
               console.log(`jsonData`,jsonData);
@@ -98,40 +76,34 @@ export default class UploadCsv extends React.Component {
     render(){
         const { classes } = this.props;
         return(
-            <div style={{textAlign:"center"}}>
-                <p>양식을 다운받아서 학생 명단을 작성한 뒤 업로드해주세요</p>
-                <Button variant="contained"><a href ='https://firebasestorage.googleapis.com/v0/b/tatabox-c2abe.appspot.com/o/students.csv?alt=media&token=3067cf63-d54c-4429-a08b-62ad62cad434' style={{textDecoration:'none', color:'black'}}>download format</a></Button>
-                
+            <div style={{textAlign:"center",fontSize:"20px"}}>
+                <p>Download the example, fill out the list of students, and upload it.</p>
+                <Button variant="contained" className={classes.margin}><a href ='https://firebasestorage.googleapis.com/v0/b/tatabox-c2abe.appspot.com/o/students.zip?alt=media&token=3067cf63-d54c-4429-a08b-62ad62cad434' style={{textDecoration:'none', color:'black'}}>download format</a></Button>
                 <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'}>
-                    <Button variant="contained">Upload</Button>
+                    <Button variant="contained" className={classes.margin}>Upload</Button>
                 </ReactFileReader>
+                <p style={{color:'#bfbfbf'}}>{this.state.filename}</p>
                 <ReactFileReader handleFiles={this.handleImages} fileTypes={'image/*'} multipleFiles={true}>
-                    <Button variant="contained" color="primary">Upload Images</Button>
+                    <Button variant="contained" color="primary" className={classes.margin}>Upload Images</Button>
                 </ReactFileReader>
                 <input accept="image/*" type="file" name="file" id="file" class="inputfile" onChange={this.handleImages} />
-                {this.state.filename}
-                <div style={{marginRight:'auto',marginLeft:'auto'}}>
-                  <p>Students List</p>
-                  <table style={{border:'1px solid #e5e5e5',fontSize:'10px',width:'500px'}}>
-                    <thead style={{display:'block'}}>
-                      <tr>
-                        <th>Student ID</th>
-                        <th>Name</th>
-                        <th>Department</th>
-                        <th>Email</th>
-                      </tr>
-                    </thead>
-                    <tbody style={{height:'100px', overflowY:'scroll', display:'block'}}>
-                      {this.state.preview}
-                    </tbody>
-                  </table>
+                <Paper id = 'row_student' style={{backgroundColor:"#bfbfbf"}}>
+                <div class='wrap'>
+                    <div class = "cell_sid">Student ID</div>
+                    <div class = "cell_name">Name</div>
+                    <div class = "cell_dept">Department</div>
+                    <div class = "cell_email">Email</div>
                 </div>
-                
-                <Button variant="contained" color="secondary" onClick={this.props.moveStep}>
+                </Paper>
+                <div style={{overflow: 'auto', height: '100px', display: 'block', flex:1, }}>
+                  {this.state.preview}
+                </div>      
+                <Button variant="contained" color="secondary" className={classes.margin} onClick={this.props.moveStep}>
                     Back
                 </Button>
-                <Button variant="contained" color="primary" onClick={()=>this.props.onSubmit(this.state.students,this.state.imgfiles)}>Submit</Button>
+                <Button variant="contained" color="primary" className={classes.margin} onClick={()=>this.props.onSubmit(this.state.students,this.state.imgfiles)}>Submit</Button>
             </div>
         )
     }
 }
+export default withStyles(styles)(UploadCsv);

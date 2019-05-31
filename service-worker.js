ce6fe0bc7518fff9d36eab694a677257973893b5
mseok <mshmjp02@gmail.com>
2019-05-31 17:53:42 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/TATABOX/precache-manifest.d7719db784f7489219e2ea57d053be30.js"
+  "/TATABOX/precache-manifest.2a56969fcf5426843d44fcdd14136e7f.js"
 );
 
 self.addEventListener('message', (event) => {
